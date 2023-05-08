1ce0811eb10bbf62d2291b478e56280115fc66a1
roseterry <r.terry125@gmail.com>
2023-05-08 14:57:44 -0400

    added comment for checking number of observations removed before running lmer; removed double comment about aov

--- CURE21-22_Terry.R
+++ CURE21-22_Terry.R
@@ -167,13 +167,12 @@ summary(aov_InvadedRemoved)
 ANPP_NA_Removed <- NPP_Join[-which(is.na(NPP_Join$total_ANPP_g) | is.na(NPP_Join$overall_group)),]
 ANPP_NA_Removed <- ANPP_NA_Removed[-which(is.na(NPP_Join$overall_group)),]
 
+# checking to see how many observations were removed
 length(NPP_Join$overall_group) - length(ANPP_NA_Removed$overall_group)
 
 #lmer of total ANPP by treatment, includes weight of invader biomass removed as random effect
 lmer_allpots <- lmer(ANPP_NA_Removed$total_ANPP_g ~ ANPP_NA_Removed$overall_group + (1|ANPP_NA_Removed$biomass_removed)) # excludes total NAs for now while Kathryn looks at the underlying issue
 summary(lmer_allpots)
-
-#aov of total NPP by treatment; doesn't account for early season invasion
 
 
 #### Total ANPP Figures ####
